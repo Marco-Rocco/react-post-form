bb4b4ed3a513d391d5fb89b6297bf3c1064a0662
Marco-Rocco <marcorocco28@icloud.com>
2025-04-14 16:22:11 +0200

    feat: change only parts fo newpost object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [newPost, setNewPost] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+    id: ""
+  })
+
+  function handleNewPost(event) {
+    const value =
+      event.target.type === "checkbox" ?
+        event.target.checked : event.target.value;
+
+    setNewPost((newPost) => ({
+      ...newPost,
+      [event.target.name]: value,
+    }))
+    return console.log(newPost)
+  }
+
 
   return (
     <>
+      <form>
 
+        <input type="text" placeholder='inserisci autore' name='author' value={newPost.author} onChange={handleNewPost} /> <br />
+        <input type="text" placeholder='inserisci titolo' name='title' value={newPost.title} onChange={handleNewPost} /><br />
+        <input type="text" placeholder='inserisci testo del post' name='body' value={newPost.body} onChange={handleNewPost} /><br />
+        <span>pubblico</span><input type="checkbox" name='public' value={newPost.public} onChange={handleNewPost} />
+        button
+
+      </form>
     </>
   )
 }
